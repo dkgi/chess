3e2a1e22488aa219774574da4c847e98f717abf6
Dominik Gabi <dominik@fb.com>
2021-06-04 20:09:57 -0700

    Specify rules for pawn move

--- chess/chess.py
+++ chess/chess.py
@@ -22,46 +22,110 @@ class Color(Enum):
         else:
             return "white"
 
+    def inverse(self) -> Color:
+        if self == Color.BLACK:
+            return Color.WHITE
+        else:
+            return Color.BLACK
 
-class Figure:
-    def __init__(self, color: Color) -> None:
-        self.color: Final[Color] = color
+
+class Field:
+    def __init__(self, color: Color | None) -> None:
+        self.color: Final[Color | None] = color
 
     def __str__(self) -> str:
+        if self.color is None:
+            return " "
+
         color = "\033[30m" if self.color == Color.BLACK else "\033[37m"
-        return f"{color}{self.name()}"
+        return f"{color}{self.name()}\033[0m"
+
+    def is_empty(self) -> bool:
+        return self.color == None
 
     @abc.abstractmethod
     def name(self) -> str:
         pass
 
+    @abc.abstractmethod
+    def can_move_to(self, board: Board, from_: Position) -> list[Position]:
+        return []
 
-class Pawn(Figure):
+
+class Empty(Field):
+    def __init__(self) -> None:
+        super().__init__(color=None)
+
+    def can_move_to(self, board: Board, from_: Position) -> list[Position]:
+        return []
+
+
+class Pawn(Field):
     def name(self) -> str:
         return "P"
 
+    def can_move_to(self, board: Board, from_: Position) -> list[Position]:
+        candidates = []
 
-class Rook(Figure):
+        direction = 1 if self.color == Color.WHITE else -1
+
+        # Forward
+        advance_one = from_.translate(row_offset=direction)
+        if advance_one is not None and board[advance_one].is_empty():
+            candidates.append(advance_one)
+
+            if (self.color == Color.WHITE and from_.row == 1) or (
+                self.color == Color.BLACK and from_.row == 6
+            ):
+                advance_two = advance_one.translate(row_offset=direction)
+                if advance_two is not None and board[advance_two].is_empty():
+                    candidates.append(advance_two)
+
+        # Strike
+        advance_left = from_.translate(row_offset=direction, column_offset=-1)
+        if (
+            advance_left is not None
+            and board[advance_left].color == self.color.inverse()
+        ):
+            candidates.append(advance_left)
+
+        advance_right = from_.translate(row_offset=direction, column_offset=1)
+        if (
+            advance_right is not None
+            and board[advance_right].color == self.color.inverse()
+        ):
+            candidates.append(advance_right)
+
+        # En passant
+        # TODO: not implemented
+
+        # Queen conversion
+        # TODO: not implemented
+
+        return candidates
+
+
+class Rook(Field):
     def name(self) -> str:
         return "R"
 
 
-class King(Figure):
+class King(Field):
     def name(self) -> str:
         return "K"
 
 
-class Queen(Figure):
+class Queen(Field):
     def name(self) -> str:
         return "Q"
 
 
-class Knight(Figure):
+class Knight(Field):
     def name(self) -> str:
         return "N"
 
 
-class Bishop(Figure):
+class Bishop(Field):
     def name(self) -> str:
         return "B"
 
@@ -90,6 +154,23 @@ class Position:
     def __str__(self) -> str:
         return f"{chr(self.column + ord('A'))}{chr(self.row + ord('1'))}"
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Position):
+            return False
+
+        return self.row == other.row and self.column == other.column
+
+    def translate(
+        self, *, row_offset: int | None = 0, column_offset: int | None = 0
+    ) -> Position | None:
+        row = self.row + row_offset
+        column = self.column + column_offset
+
+        if row in range(8) and column in range(8):
+            return Position(row, column)
+
+        return None
+
 
 class InvalidMove(Exception):
     pass
@@ -97,34 +178,34 @@ class InvalidMove(Exception):
 
 class Board:
     def __init__(self) -> None:
-        # self._fields[1][a] is bottom left, self._fields[1][h] is bottom right
-        self._fields: list[list[Figure | None]] = []
+        # self._board[1][a] is bottom left, self._board[1][h] is bottom right
+        self._board: list[list[Field]] = []
         for row_index in range(8):
-            self._fields.append([None] * 8)
+            self._board.append([Empty()] * 8)
 
-        self._fields[0][4] = King(Color.WHITE)
-        self._fields[7][4] = King(Color.BLACK)
+        self._board[0][4] = King(Color.WHITE)
+        self._board[7][4] = King(Color.BLACK)
 
-        self._fields[0][3] = Queen(Color.WHITE)
-        self._fields[7][3] = Queen(Color.BLACK)
+        self._board[0][3] = Queen(Color.WHITE)
+        self._board[7][3] = Queen(Color.BLACK)
 
-        self._fields[0][2] = Bishop(Color.WHITE)
-        self._fields[7][2] = Bishop(Color.BLACK)
-        self._fields[0][5] = Bishop(Color.WHITE)
-        self._fields[7][5] = Bishop(Color.BLACK)
+        self._board[0][2] = Bishop(Color.WHITE)
+        self._board[7][2] = Bishop(Color.BLACK)
+        self._board[0][5] = Bishop(Color.WHITE)
+        self._board[7][5] = Bishop(Color.BLACK)
 
-        self._fields[0][1] = Knight(Color.WHITE)
-        self._fields[7][1] = Knight(Color.BLACK)
-        self._fields[0][6] = Knight(Color.WHITE)
-        self._fields[7][6] = Knight(Color.BLACK)
+        self._board[0][1] = Knight(Color.WHITE)
+        self._board[7][1] = Knight(Color.BLACK)
+        self._board[0][6] = Knight(Color.WHITE)
+        self._board[7][6] = Knight(Color.BLACK)
 
-        self._fields[0][0] = Rook(Color.WHITE)
-        self._fields[7][0] = Rook(Color.BLACK)
-        self._fields[0][7] = Rook(Color.WHITE)
-        self._fields[7][7] = Rook(Color.BLACK)
+        self._board[0][0] = Rook(Color.WHITE)
+        self._board[7][0] = Rook(Color.BLACK)
+        self._board[0][7] = Rook(Color.WHITE)
+        self._board[7][7] = Rook(Color.BLACK)
 
-        self._fields[1] = [Pawn(Color.WHITE)] * 8
-        self._fields[6] = [Pawn(Color.BLACK)] * 8
+        self._board[1] = [Pawn(Color.WHITE)] * 8
+        self._board[6] = [Pawn(Color.BLACK)] * 8
 
     def __str__(self) -> str:
         light = "\u001b[42m"
@@ -133,7 +214,7 @@ class Board:
 
         lines = ["  ABCDEFGH"]
 
-        for row_index, row in enumerate(self._fields):
+        for row_index, row in enumerate(self._board):
             line = f"{row_index + 1} "
             for field_index, field in enumerate(row):
                 color = dark if (row_index + field_index) % 2 == 0 else light
@@ -142,14 +223,29 @@ class Board:
 
         return "\n".join(reversed(lines))
 
-    def move(self, *, color: Color, from_: Position, to: Position) -> None:
-        field = self._fields[from_.row][from_.column]
-        if field is None or field.color != color:
-            raise InvalidMove(f"No {color} piece at {from_}")
+    def __getitem__(self, key: Position) -> Field:
+        return self._board[key.row][key.column]
 
-        # TODO: actual validation if move is valid
-        self._fields[to.row][to.column] = field
-        self._fields[from_.row][from_.column] = None
+    def __setitem__(self, key: Position, value: Field) -> None:
+        self._board[key.row][key.column] = value
+
+    def move(self, *, moving_color: Color, from_: Position, to: Position) -> None:
+        field = self[from_]
+
+        if field.color != moving_color:
+            raise InvalidMove(f"no {color} piece at {from_}")
+
+        if self[to].color == moving_color:
+            raise InvalidMove(f"cannot move to field with {moving_color} piece")
+
+        if to not in field.can_move_to(self, from_):
+            raise InvalidMove(f"{field} cannot move from {from_} to {to}")
+
+        if self[to].color not in [moving_color, None]:
+            LOG.info(f"{self[from_]} at {from_} strikes {self[to]} at {to}")
+
+        self[to] = field
+        self[from_] = Empty()
 
 
 class Game:
@@ -157,18 +253,17 @@ class Game:
         self._board: Board = Board()
         self._turn = Color.WHITE
 
-    def show_board(self) -> None:
-        print(f"{self._board}")
+    def __str__(self) -> str:
         color = "White" if self._turn == Color.WHITE else "Black"
-        print(f"{color}'s turn.")
+        return f"{self._board}\n{color}'s turn."
 
     def move(self, *, from_: Position, to: Position) -> None:
         try:
-            self._board.move(color=self._turn, from_=from_, to=to)
-            self._turn = Color.WHITE if self._turn == Color.BLACK else Color.BLACK
-            self.show_board()
+            self._board.move(moving_color=self._turn, from_=from_, to=to)
+            self._turn = self._turn.inverse()
+            LOG.info(f"{self}")
         except InvalidMove as error:
-            print(f"Invalid move: {error}")
+            LOG.error(f"Invalid move: {error}")
 
 
 HELP = """\
@@ -195,23 +290,23 @@ class InvalidCommand(Exception):
 
 
 def main() -> None:
-    print("Chess 0.1. Type `help` for instructions.")
+    LOG.info("Chess 0.1. Type `help` for instructions.")
     game = None
     while True:
         try:
             command = input(">> ").lower()
 
             if command == "exit":
-                print("Exiting...")
+                LOG.info("Exiting...")
                 break
 
             if command == "help":
-                print(HELP)
+                LOG.info(HELP)
                 continue
 
             if command == "new":
                 game = Game()
-                game.show_board()
+                LOG.info(f"{game}")
                 continue
 
             if command == "debug":
@@ -219,7 +314,7 @@ def main() -> None:
 
                 pdb.set_trace()
 
-            if command.startswith("move "):
+            if command.startswith("m ") or command.startswith("move "):
                 if game is None:
                     raise InvalidCommand(
                         command, "No game in progress, start game with `new`."
@@ -238,10 +333,10 @@ def main() -> None:
 
             raise InvalidCommand(command)
         except InvalidCommand as error:
-            print(f"Invalid command: {error}\nEnter `help` for instructions.")
+            LOG.info(f"Invalid command: {error}\nEnter `help` for instructions.")
             continue
         except KeyboardInterrupt:
-            print("\nExiting...")
+            LOG.info("\nExiting...")
             break
 
 
